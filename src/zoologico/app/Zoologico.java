cff0108f9c5ac6761857cdf014b5ec9ac4d38301
julian-guerra92 <julianguerra92@gmail.com>
2023-03-13 20:08:20 -0500

    Configuración inicial de las clases del proyecto

---
+++ src/zoologico/app/Zoologico.java
@@ -0,0 +1,18 @@
+package zoologico.app;
+
+import zoologico.modelos.*;
+
+import java.util.ArrayList;
+
+public class Zoologico {
+    public static ArrayList<WildAnimal> inventoryWildAnimal = new ArrayList<WildAnimal>();
+    public static ArrayList<DomesticAnimal> inventoryDomesticAnimal = new ArrayList<DomesticAnimal>();
+    public static ArrayList<AdoptionAnimal> inventoryAdoptionAnimal = new ArrayList<AdoptionAnimal>();
+    public static ArrayList<Customer> customerList = new ArrayList<Customer>();
+    public static ArrayList<Plans> plansList = new ArrayList<Plans>();
+    public static ArrayList<SalesInvoice> invoiceList = new ArrayList<SalesInvoice>();
+
+
+    public static void main(String[] args) {
+    }
+}
--- src/zoologico/modelos/Animal.java
+++ src/zoologico/modelos/Animal.java
@@ -1,36 +1,91 @@
 package zoologico.modelos;
 
-public class Animal {
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
-    private int id;
-    private String name;
+/**
+ * @author  Julian A. Rodríguez G. - Oscar Albeiro Blandón
+ * @version 0.0.1
+ * Esta clase representa la estructura y carcaterística general de los animales pertenecientes
+ * al zoológico. De esta se extenderá las demás clases de los tipos específicos de animales en el zoológico.
+ */
+public class Animal {
+    private String id, scientificName, species;
+    private LocalDate birthDate;
+    private long age;
 
     public Animal() {
     }
 
-    public Animal(int id, String name) {
+    public Animal(String id, String scientificName, String species, LocalDate birthDate) {
         this.id = id;
-        this.name = name;
+        this.scientificName = scientificName;
+        this.species = species;
+        this.birthDate = birthDate;
+        this.age = ChronoUnit.YEARS.between(LocalDate.now(), birthDate);
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getScientificName() {
+        return scientificName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public String eat () {
+        return "El animal se encuentra comiendo actualmente!";
+    }
+
+    public String drinkingWater(){
+        return "Ahora el animal se encuentra bebiendo agua!";
+    }
+
+    public String sleep(){
+        return "Silecio. Animal durmiendo!";
     }
 
     @Override
     public String toString() {
-        return "zoologico.modelos.Animal{" + "id=" + id + ", name='" + name + '\'' + '}';
+        return "Animal{" +
+                "id='" + id + '\'' +
+                ", scientificName='" + scientificName + '\'' +
+                ", species='" + species + '\'' +
+                ", birthDate=" + birthDate +
+                ", age=" + age +
+                '}';
     }
+
 }
